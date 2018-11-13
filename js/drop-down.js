c6f7a1d66c8b91a6063f130568e470efd4da9d85
andriy9191 <andriy9191@gmail.com>
2018-11-13 16:04:46 +0200

    Заканчивает адаптип Оформления заказов

--- js/drop-down.js
+++ js/drop-down.js
@@ -4,23 +4,39 @@
 (function () {
   var body = document.querySelector('body');
   var dropMenu = body.querySelectorAll('.drop-down');
-  var dropMenuContainer = body.querySelectorAll('.drop-down__list-wrapper');
   var dropMenuButton = body.querySelectorAll('.drop-down__button');
 
 
   Array.from(dropMenu).forEach(function (item, index) {
     item.addEventListener('click', function (evt) {
-      if (evt.target.closest('.drop-down__button')) {
-        dropMenuContainer[index].classList.toggle('drop-down__list-wrapper--active');
+      var list = item.querySelector('.drop-down__list');
+      var search = item.querySelector('.drop-down__label-search');
+      var isButton = evt.target.closest('.drop-down__button');
+      var isLink = evt.target.closest('.drop-down__link');
+      var isSearch = evt.target.closest('.drop-down__link--search');
+
+      list.classList.remove('drop-down__list--hidden');
+
+      if (isButton) {
+        list.classList.toggle('drop-down__list--active');
         dropMenuButton[index].classList.toggle('drop-down__button--active');
         document.addEventListener('keydown', modalEscPressHandler);
       }
-      if (evt.target.closest('.drop-down__link')) {
+      if (isLink && !isSearch) {
         evt.preventDefault();
-        dropMenuContainer[index].classList.remove('drop-down__list-wrapper--active');
+        list.classList.remove('drop-down__list--active');
+        dropMenuButton[index].classList.remove('drop-down__button--active');
+        search.classList.remove('drop-down__label-search--active');
+        dropMenuButton[index].textContent = evt.target.textContent;
+        document.removeEventListener('keydown', modalEscPressHandler);
+      }
+      if (isSearch) {
+        evt.preventDefault();
+        list.classList.remove('drop-down__list--active');
         dropMenuButton[index].classList.remove('drop-down__button--active');
         dropMenuButton[index].textContent = evt.target.textContent;
-
+        search.classList.add('drop-down__label-search--active');
+        search.focus();
         document.removeEventListener('keydown', modalEscPressHandler);
       }
     })
@@ -36,7 +52,7 @@
 
     Array.from(addActiveButton).forEach(function (item) {
       item.classList.remove('drop-down__button--active');
-      item.nextSibling.nextSibling.classList.remove('drop-down__list-wrapper--active');
+      item.nextSibling.nextSibling.querySelector('.drop-down__list--active').classList.remove('drop-down__list--active');
       document.removeEventListener('keydown', modalEscPressHandler);
     });
   };
